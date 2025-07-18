57fd0ef3fb9e13d36e4af9929a5e8b555604b5bb
davo <davcho2014@gmail.com>
2025-07-18 06:38:55 +0100

    updated state file block

--- state.tf
+++ state.tf
@@ -2,7 +2,13 @@ terraform {
   backend "s3" {
     bucket = "my-first-practice-bucket-aaa"
     key = "statefile/terraform.tfstate"
-    region = var.region
-    use_lockfile = true
+    #profile = "terraform-user"
+    region = "eu-west-2"
+    #use_lockfile = true
   }
 }
+
+# Error: Variables not allowed
+# │ 
+# │   on state.tf line 5, in terraform:
+# │    5:     region = var.region
