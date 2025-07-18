1075aadf96d56d5eb76d1446e2134afd80d115be
davo <davcho2014@gmail.com>
2025-07-18 15:20:10 +0100

    yaml files

--- terraform/state.tf
+++ terraform/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-first-practice-bucket-aaa"
-    key = "statefile/terraform.tfstate"
+    key    = "statefile/terraform.tfstate"
     #profile = "terraform-user"
     region = "eu-west-2"
     #use_lockfile = true
