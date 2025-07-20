733433d7a6126d5c72ac09860eec7012af72d394
davo <davcho2014@gmail.com>
2025-07-20 19:34:14 +0100

    updated terraform version in test_oidc yaml file

--- terraform/state.tf
+++ terraform/state.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket = "my-first-practice-bucket-aaa"
     key    = "statefile/terraform.tfstate"
-    #profile = "terraform-user"
-    region = "eu-west-2"
+    ##profile = "terraform-user"
+    region       = "eu-west-2"
     use_lockfile = true
   }
 }
